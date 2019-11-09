a9bce28de1f2eac599d2d866fb6d93099d660777
sai teja <saitejasunkari04@gmail.com>
2019-11-08 20:26:43 -0500

    show bill added 39

--- C0765775_MidTerm_MAD3115F2019/ShowBillDetailsViewController.swift
+++ C0765775_MidTerm_MAD3115F2019/ShowBillDetailsViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class ShowBillDetailsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var tableView: UITableView!
+        
+        
+        var billDetailsArray = [Bill]()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupUI()
+        }
+        
+        
+        func setupUI(){
+            tableView.tableFooterView = .init()
+        }
+        @IBAction func addNewBillTapped(_ sender: Any) {
+            if let addbillVC = self.storyboard?.instantiateViewController(identifier: "AddNewBillViewController") as? AddNewBillViewController{
+                addbillVC.delegate = self
+                self.navigationController?.pushViewController(addbillVC, animated: true)
+            }
+        }
+        
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
-}
